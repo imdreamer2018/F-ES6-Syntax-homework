c448f988d990d8d8893e4cf9077872a07e73b228
imdreamer <743295483@qq.com>
2020-10-15 21:39:29 +0800

    [yangqian] complete: practice2.

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,7 @@
-const inject
+const inject = (items, sections) => {
+  sections.reverse().map(item => {
+    items.splice(item.index, 0, item.content)
+  });
+  return items;
+};
 export { inject };
