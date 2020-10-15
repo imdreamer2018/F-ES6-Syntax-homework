010e6577f40e06213c2ab1df63ab2233c22efc97
imdreamer <743295483@qq.com>
2020-10-15 21:13:45 +0800

    [yangqian] complete: practice1.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = ({ data , column }) => {
+  let columns = [];
+  column.map(o => {
+    columns.push(o.name);
+  });
+  return data.map(info => {
+    let result = info.reduce((result, value, index) => {
+      result[columns[index]] = value;
+      return result;
+    }, {});
+    return result;
+  })
+};
 export { parseData };
